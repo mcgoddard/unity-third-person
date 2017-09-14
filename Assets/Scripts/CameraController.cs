e276a32358ec7feb9ffe2df738048687709e0ead
Michael Goddard <mikeygiom@gmail.com>
2017-09-14 23:16:49 +0100

    Added a building to the scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    private GameObject player;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        var position = player.transform.position;
+        position.x -= 3;
+        position.y += 3;
+        position.z -= 3;
+        transform.position = position;
 	}
 }
