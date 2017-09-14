fa6f0dbe81f6c74d63c2efb746e8223ff72ff06a
Michael Goddard <mikeygiom@gmail.com>
2017-09-14 23:23:16 +0100

    Modified the house shape slightly. Cube looks at the camera.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -68,5 +68,15 @@ public class PlayerController : MonoBehaviour {
             position.z -= (speed * Time.deltaTime);
         }
         transform.position = position;
+        
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
