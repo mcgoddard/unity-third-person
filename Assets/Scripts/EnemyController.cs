aeef4d9be0aef1ed639815477433915167607c00
Mike Goddard <mikeygiom@gmail.com>
2017-09-17 22:45:19 +0100

    Enemy has vision cone that's visibility can be toggled by pressing 'L'. Will detect the player within the cone and change state (currently just stops). Does not take cover into account.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,17 +7,18 @@ public class EnemyController : MonoBehaviour {
 	public enum EnemyState
 	{
 		Patrolling,
-		Investigating,
 		Attacking
 	}
 
 	private EnemyState currentState = EnemyState.Patrolling;
-	private Vector3 investigationPoint;
 	private Vector3[] patrollingPoints;
 	private float currentWaitingTime;
 	private float[] patrollingWaitTimes;
 	private int patrollingIndex = 0;
 	private NavMeshAgent agent;
+    private VisionCone leftCone;
+    private VisionCone rightCone;
+    private GameObject player;
 
 	// Use this for initialization
 	void Start () 
@@ -38,6 +39,9 @@ public class EnemyController : MonoBehaviour {
 		};
 		agent = GetComponent<NavMeshAgent>();
 		agent.speed = 1.5f;
+        leftCone = transform.Find("LeftVisionCone").GetComponent<VisionCone>();
+        rightCone = transform.Find("RightVisionCone").GetComponent<VisionCone>();
+        player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
@@ -45,23 +49,55 @@ public class EnemyController : MonoBehaviour {
 	{
 		switch (currentState) 
 		{
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                agent.isStopped = true;
+                break;
 			case EnemyState.Patrolling:
-				float distance = Vector3.Magnitude(transform.position - patrollingPoints[patrollingIndex]);
-				if (distance < 0.1)
-				{
-					if (currentWaitingTime < patrollingWaitTimes[patrollingIndex])
-					{
-						currentWaitingTime += Time.deltaTime;
-					}
-					else
-					{
-						currentWaitingTime = 0;
-						patrollingIndex++;
-						patrollingIndex = patrollingIndex % patrollingPoints.Length;
-						agent.destination = patrollingPoints[patrollingIndex];
-					}
-				}
+                if (PointInTriangle(player.transform.position, transform.position, leftCone.GetConeEnd(), rightCone.GetConeEnd()) && 
+                    CanRayCastPlayer(transform.position, player.transform.position))
+                {
+                    currentState = EnemyState.Attacking;
+                }
+                else 
+                {
+    				float distance = Vector3.Magnitude(transform.position - patrollingPoints[patrollingIndex]);
+    				if (distance < 0.1)
+    				{
+    					if (currentWaitingTime < patrollingWaitTimes[patrollingIndex])
+    					{
+    						currentWaitingTime += Time.deltaTime;
+    					}
+    					else
+    					{
+    						currentWaitingTime = 0;
+    						patrollingIndex++;
+    						patrollingIndex = patrollingIndex % patrollingPoints.Length;
+    						agent.destination = patrollingPoints[patrollingIndex];
+    					}
+    				}
+                }
 				break;
 		}
 	}
+
+    // Helper to decide if two points are on the same side of a line
+    private static bool SameSide(Vector3 p1, Vector3 p2, Vector3 a, Vector3 b)
+    {
+        var cp1 = Vector3.Cross(b - a, p1 - a);
+        var cp2 = Vector3.Cross(b - a, p2 - a);
+        return Vector3.Dot(cp1, cp2) >= 0;
+    }
+
+    // Helper to decide if a point is within a triangle
+    private static bool PointInTriangle(Vector3 p, Vector3 a, Vector3 b, Vector3 c)
+    {
+        return (SameSide(p, a, b, c) && SameSide(p, b, a, c) && SameSide(p, c, a, b));
+    }
+
+    // Helper to check for a raycast hit to the player (are they behind cover?)
+    private static bool CanRayCastPlayer(Vector3 position, Vector3 playerPosition)
+    {
+        return true;
+    }
 }
---
+++ Assets/Scripts/VisionCone.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionCone : MonoBehaviour {
+    public bool Left = false;
+    public bool Visible = true;
+
+    private LineRenderer line;
+
+	// Use this for initialization
+	void Start () 
+    {
+        line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            Visible = !Visible;
+        }
+        if (Visible)
+        {
+            Vector3[] positions = new Vector3[line.positionCount];
+            line.GetPositions(positions);
+            var start = transform.position;
+            var end = transform.position;
+            if (Left)
+            {
+                end.x += 5;
+            }
+            else
+            {
+                end.x += -5;
+            }
+            end.z += 4;
+            positions[0] = start;
+            positions[1] = end;
+            var direction = positions[1] - positions[0];
+            direction = transform.parent.rotation * direction;
+            positions[1] = direction + positions[0];
+            line.SetPositions(positions);
+        }
+        else
+        {
+            Vector3[] positions = new Vector3[]
+            {
+                transform.position,
+                transform.position
+            };
+            line.SetPositions(positions);
+        }
+	}
+
+    public Vector3 GetConeEnd()
+    {
+        return line.GetPosition(1);
+    }
+}
