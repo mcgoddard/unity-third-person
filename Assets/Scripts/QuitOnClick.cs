b1e20d8723193364677df125c7a5d1283bdaf47c
Jacques Woodmaster Coney <jacques.coney1@gmail.com>
2017-09-22 18:17:08 +0100

    added main menu example to game

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+    public void Quit()
+    {
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObjected;
+
+    private bool buttonSelected;
+
+    void Update() {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObjected);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
